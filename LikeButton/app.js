515adfcca0c370ae8f17288ffd1ea0538739f6e9
Owen Yoshishige <owenyoshishige@gmail.com>
2022-08-30 16:54:01 -0700

    create like button

---
+++ LikeButton/app.js
@@ -0,0 +1,33 @@
+const ctr = document.getElementById('container');
+const btn = document.createElement('button');
+const btnText = '<i class="fa-solid fa-heart"></i> Like'
+let liked = false;
+
+btn.classList.add('btn', 'like-btn');
+btn.innerHTML = btnText;
+
+btn.addEventListener('click', function() {
+  changeLikedState(this);
+});
+
+const changeLikedState = (button) => {
+  button.innerHTML = '<img src="https://www.freeiconspng.com/uploads/load-icon-png-27.png" width="12" alt="Load Symbols" /> Like';
+
+  if (!liked) {
+    button.classList.replace('like-btn', 'like-btn-loading');
+    setTimeout(() => {
+      button.classList.replace('like-btn-loading', 'like-btn-liked');
+      liked = !liked;
+      button.innerHTML = btnText;
+    }, 500);
+  } else {
+    button.classList.replace('like-btn-liked', 'like-btn-loading');
+    setTimeout(() => {
+      button.classList.replace('like-btn-loading', 'like-btn');
+      liked = !liked;
+      button.innerHTML = btnText;
+    }, 500);
+  }
+};
+
+ctr.appendChild(btn);
