0eb8ffb137f10ab690efe9f34b29c939e92de25c
Owen Yoshishige <owenyoshishige@gmail.com>
2022-09-02 15:59:23 -0700

    complete input world

--- InputWorld/app.js
+++ InputWorld/app.js
@@ -1,6 +1,41 @@
-const inp = document.getElementById('name');
+const input = document.querySelector('.name');
+const normalDisplay = document.querySelector('.normal-output');
+const debounceDisplay = document.querySelector('.debounce-output');
+const throttleDisplay = document.querySelector('.throttle-output');
 
-inp.addEventListener('input', (e) => {
-  console.log(e.target.value)
-  // console.log(e.target.textContent)
+input.addEventListener('input', () => {
+  normalDisplay.textContent = input.value;
+  debounceInput(input.value, 1000);
+  throttleInput(input.value, 1000);
 });
+
+const debounce = () => {
+  let timeoutID;
+
+  return (inp, delay) => {
+    clearTimeout(timeoutID);
+
+    timeoutID = setTimeout(() => {
+      timeoutID = null;
+      debounceDisplay.textContent = inp;
+    }, delay);
+  };
+};
+
+const throttle = () => {
+  let timeoutID;
+  let currentInp;
+
+  return (inp, delay) => {
+    currentInp = inp;
+    if (timeoutID) return;
+
+    timeoutID = setTimeout(() => {
+      timeoutID = null;
+      throttleDisplay.textContent = currentInp;
+    }, delay);
+  }
+};
+
+const debounceInput = debounce();
+const throttleInput = throttle();
